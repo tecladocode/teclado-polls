4006560c12bc6c28a856b4651ce16abe56734047
Jose Salvatierra <jslvtr@gmail.com>
2019-12-16 21:18:38 +0000

    Swap over to postgres.

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from urllib.parse import quote
 import os
-import sqlite3
+import psycopg2
 from flask import Flask, render_template, request, session, redirect, url_for
 import requests
 from secrets import CLIENT_SECRET
@@ -13,6 +13,8 @@ app.secret_key = "jose"
 DISCORD_AUTH_URL = "https://discordapp.com/api/oauth2/authorize"
 DISCORD_TOKEN_URL = "https://discordapp.com/api/oauth2/token"
 DISCORD_GET_CURRENT_USER = "https://discordapp.com/api/users/@me"
+
+DATABASE_URI = os.environ.get("DATABASE", "postgres://postgres:1234@localhost:5432/polling")
 
 CLIENT_ID = "654393272189321237"
 DISCORD_REDIRECT_URI = os.environ.get("DISCORD_AUTH_REDIRECT", "http://127.0.0.1:5000/authorize")
@@ -53,14 +55,17 @@ def authorize():
     if session.get('poll_id'):
         return redirect(url_for('poll', poll_id=session['poll_id']))
 
-    with sqlite3.connect("data.db") as connection:
-        latest_poll = connection.execute("SELECT * FROM polls ORDER BY poll_id DESC LIMIT 1").fetchone()
-        if not latest_poll:
-            return redirect(url_for("create_poll"))
+    with psycopg2.connect(DATABASE_URI) as connection:
+        with connection.cursor() as cur:
+            cur.execute("SELECT * FROM polls ORDER BY poll_id DESC LIMIT 1")
+            latest_poll = cur.fetchone()
+            if not latest_poll:
+                return redirect(url_for("create_poll"))
 
-        latest_poll_id = latest_poll[0]
-        poll = connection.execute("SELECT * FROM polls JOIN options on polls.poll_id = options.poll_id WHERE polls.poll_id = ?", (latest_poll_id,)).fetchall()
-        title = poll[0][1]
+            latest_poll_id = latest_poll[0]
+            cur.execute("SELECT * FROM polls JOIN options on polls.poll_id = options.poll_id WHERE polls.poll_id = %s", (latest_poll_id,))
+            poll = cur.fetchall()
+            title = poll[0][1]
 
     return render_template("latest.html", title=title, poll=poll, poll_id=latest_poll_id, discord_uri=FINAL_URI)
 
@@ -68,39 +73,43 @@ def authorize():
 @app.route("/poll/<int:poll_id>")
 def poll(poll_id):
     session['poll_id'] = poll_id
-    with sqlite3.connect("data.db") as connection:
-        poll = connection.execute("SELECT * FROM polls JOIN options on polls.poll_id = options.poll_id WHERE polls.poll_id = ?", (poll_id,)).fetchall()
-        title = poll[0][1]
+    with psycopg2.connect(DATABASE_URI) as connection:
+        with connection.cursor() as cur:
+            cur.execute("SELECT * FROM polls JOIN options on polls.poll_id = options.poll_id WHERE polls.poll_id = %s", (poll_id,))
+            poll = cur.fetchall()
+            title = poll[0][1]
     return render_template("latest.html", title=title, poll=poll, poll_id=poll_id, discord_uri=FINAL_URI)
 
 
 @app.route("/vote/<int:poll_id>", methods=["POST"])
 def vote(poll_id):
     selected_vote = request.form.get("vote")
-    with sqlite3.connect("data.db") as connection:
-        connection.execute(
-            "INSERT INTO votes (username, discriminator, vote, poll_id) VALUES (?, ?, ?, ?)",
-            (session['username'], session['discriminator'], selected_vote, poll_id)
-        )
+    with psycopg2.connect(DATABASE_URI) as connection:
+        with connection.cursor() as cur:
+            cur.execute(
+                "INSERT INTO votes (username, discriminator, vote, poll_id) VALUES (%s, %s, %s, %s)",
+                (session['username'], session['discriminator'], selected_vote, poll_id)
+            )
 
     return f"You voted for {selected_vote}"
 
 
 @app.route("/view/<int:poll_id>")
 def view_poll(poll_id):
-    with sqlite3.connect("data.db") as connection:
-        cursor = connection.execute("SELECT * FROM polls WHERE poll_id = ?", (poll_id,))
-        poll = cursor.fetchone()
-        owner_viewing = False
-        if session.get("username") and session.get("discriminator"):
-            owner_viewing = poll[2] == session['username'] and poll[3] == session['discriminator']
+    with psycopg2.connect(DATABASE_URI) as connection:
+        with connection.cursor() as cur:
+            cur.execute("SELECT * FROM polls WHERE poll_id = %s", (poll_id,))
+            poll = cur.fetchone()
+            owner_viewing = False
+            if session.get("username") and session.get("discriminator"):
+                owner_viewing = poll[2] == session['username'] and poll[3] == session['discriminator']
 
-        votes_cursor = connection.execute("""
-            SELECT options.option_id, options.option_text, COUNT(votes.vote) as count, COUNT(votes.vote) * 100.0 / sum(count(votes.vote)) over() as percentage FROM options
-            LEFT JOIN votes on options.option_id = votes.vote
-            WHERE options.poll_id = ?
-            GROUP BY options.option_text;""", (poll_id,))
-        votes = votes_cursor.fetchall()
+            cur.execute("""
+                SELECT options.option_id, options.option_text, COUNT(votes.vote) as count, COUNT(votes.vote) * 100.0 / sum(count(votes.vote)) over() as percentage FROM options
+                LEFT JOIN votes on options.option_id = votes.vote
+                WHERE options.poll_id = %s
+                GROUP BY options.option_id, options.option_text;""", (poll_id,))
+            votes = cur.fetchall()
     return render_template("view_poll.html", poll=poll, votes=votes, owner_viewing=owner_viewing, discord_uri=FINAL_URI)
 
 
@@ -110,12 +119,13 @@ def create_poll():
         title = request.form.get("title")
         option_keys = ["option1", "option2", "option3", "option4"]
 
-        with sqlite3.connect("data.db") as connection:
-            cursor = connection.execute("INSERT INTO polls (title, owner, owner_discriminator) VALUES (?, ?, ?)", (title, session['username'], session['discriminator']))
-            last_inserted_id = cursor.lastrowid
+        with psycopg2.connect(DATABASE_URI) as connection:
+            with connection.cursor() as cur:
+                cur.execute("INSERT INTO polls (title, owner, owner_discriminator) VALUES (%s, %s, %s) RETURNING poll_id", (title, session['username'], session['discriminator']))
+                last_inserted_id = cur.fetchone()[0]
 
-            options = [(request.form.get(option), last_inserted_id) for option in option_keys if request.form.get(option)]
-            connection.executemany("INSERT INTO options (option_text, poll_id) VALUES (?, ?)", options)
+                options = [(request.form.get(option), last_inserted_id) for option in option_keys if request.form.get(option)]
+                cur.executemany("INSERT INTO options (option_text, poll_id) VALUES (%s, %s)", options)
 
         return redirect(url_for("poll", poll_id=last_inserted_id))
     return render_template("create_poll.html", discord_uri=FINAL_URI)
@@ -123,11 +133,12 @@ def create_poll():
 
 @app.route("/manage_polls")
 def manage_polls():
-    with sqlite3.connect("data.db") as connection:
-        cursor = connection.execute("SELECT * FROM polls WHERE owner = ? AND owner_discriminator = ?",
-                                    (session["username"], session["discriminator"]))
+    with psycopg2.connect(DATABASE_URI) as connection:
+        with connection.cursor() as cur:
+            cur.execute("SELECT * FROM polls WHERE owner = %s AND owner_discriminator = %s",
+                                        (session["username"], session["discriminator"]))
 
-        return render_template("manage_polls.html", polls=cursor.fetchall())
+            return render_template("manage_polls.html", polls=cur.fetchall())
 
 
 @app.route("/logout")
@@ -138,8 +149,11 @@ def logout():
 
 @app.route("/pick_winner/<int:option_id>")
 def pick_winner(option_id):
-    with sqlite3.connect("data.db") as connection:
-        voters = connection.execute("SELECT * FROM votes WHERE vote = ?", (option_id,)).fetchall()
-        random_winner = connection.execute("SELECT * FROM votes WHERE vote = ? ORDER BY RANDOM() LIMIT 1;", (option_id,)).fetchone()
+    with psycopg2.connect(DATABASE_URI) as connection:
+        with connection.cursor() as cur:
+            cur.execute("SELECT * FROM votes WHERE vote = %s", (option_id,))
+            voters = cur.fetchall()
+            cur.execute("SELECT * FROM votes WHERE vote = %s ORDER BY RANDOM() LIMIT 1;", (option_id,))
+            random_winner = cur.fetchone()
 
-        return render_template("winner.html", option_id=option_id, voters=voters, winner_username=random_winner[0], winner_discriminator=random_winner[1])
+            return render_template("winner.html", option_id=option_id, voters=voters, winner_username=random_winner[0], winner_discriminator=random_winner[1])
--- create_tables.py
+++ create_tables.py
@@ -1,6 +1,10 @@
-import sqlite3
+import os
+import psycopg2
 
-with sqlite3.connect("data.db") as connection:
-    connection.execute("CREATE TABLE IF NOT EXISTS options (option_id INTEGER PRIMARY KEY, option_text TEXT, poll_id INTEGER);")
-    connection.execute("CREATE TABLE IF NOT EXISTS polls (poll_id INTEGER PRIMARY KEY, title TEXT, owner TEXT, owner_discriminator TEXT);")
-    connection.execute("CREATE TABLE IF NOT EXISTS votes (username TEXT, discriminator TEXT, vote TEXT, poll_id TEXT);")
+DATABASE_URI = os.environ.get("DATABASE", "postgres://postgres:1234@localhost:5432/polling")
+
+with psycopg2.connect(DATABASE_URI) as connection:
+    with connection.cursor() as cur:
+        cur.execute("CREATE TABLE IF NOT EXISTS options (option_id SERIAL PRIMARY KEY, option_text TEXT, poll_id INTEGER);")
+        cur.execute("CREATE TABLE IF NOT EXISTS polls (poll_id SERIAL PRIMARY KEY, title TEXT, owner TEXT, owner_discriminator TEXT);")
+        cur.execute("CREATE TABLE IF NOT EXISTS votes (username TEXT, discriminator TEXT, vote INTEGER, poll_id INTEGER);")

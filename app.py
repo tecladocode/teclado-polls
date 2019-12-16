d80ecd0a1b6f0ed6fece9c24e5ed269f5472d578
Jose Salvatierra <jslvtr@gmail.com>
2019-12-16 20:04:44 +0000

    Add a bunch of stuff.

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
+from urllib.parse import quote
+import os
 import sqlite3
-from flask import Flask, render_template, request, session
+from flask import Flask, render_template, request, session, redirect, url_for
 import requests
 from secrets import CLIENT_SECRET
 import create_tables
@@ -13,8 +15,9 @@ DISCORD_TOKEN_URL = "https://discordapp.com/api/oauth2/token"
 DISCORD_GET_CURRENT_USER = "https://discordapp.com/api/users/@me"
 
 CLIENT_ID = "654393272189321237"
+DISCORD_REDIRECT_URI = os.environ.get("DISCORD_AUTH_REDIRECT", "http://127.0.0.1:5000/authorize")
 
-FINAL_URI = f"{DISCORD_AUTH_URL}?client_id={CLIENT_ID}&redirect_uri=https%3A%2F%2Fteclado-polls.herokuapp.com%2Fauthorize&response_type=code&scope=identify"
+FINAL_URI = f"{DISCORD_AUTH_URL}?client_id={CLIENT_ID}&redirect_uri={quote(DISCORD_REDIRECT_URI)}&response_type=code&scope=identify"
 
 
 @app.route("/")
@@ -31,7 +34,7 @@ def authorize():
         'client_secret': CLIENT_SECRET,
         'grant_type': 'authorization_code',
         'code': code,
-        'redirect_uri': "https://teclado-polls.herokuapp.com//authorize",
+        'redirect_uri': DISCORD_REDIRECT_URI,
         'scope': 'identify'
     }
     headers = {
@@ -46,29 +49,37 @@ def authorize():
     session["username"] = json_data.get("username")
     session["discriminator"] = json_data.get("discriminator")
 
+    if session.get('poll_id'):
+        return redirect(url_for('poll', poll_id=session['poll_id']))
+
     with sqlite3.connect("data.db") as connection:
-        latest_poll_id = connection.execute("SELECT * FROM polls ORDER BY poll_id LIMIT 1").fetchone()[0]
+        latest_poll = connection.execute("SELECT * FROM polls ORDER BY poll_id DESC LIMIT 1").fetchone()
+        if not latest_poll:
+            return redirect(url_for("create_poll"))
+
+        latest_poll_id = latest_poll[0]
         poll = connection.execute("SELECT * FROM polls JOIN options on polls.poll_id = options.poll_id WHERE polls.poll_id = ?", (latest_poll_id,)).fetchall()
         title = poll[0][1]
 
-    return render_template("latest.html", title=title, poll=poll)
+    return render_template("latest.html", title=title, poll=poll, poll_id=latest_poll_id, discord_uri=FINAL_URI)
 
 
 @app.route("/poll/<int:poll_id>")
 def poll(poll_id):
+    session['poll_id'] = poll_id
     with sqlite3.connect("data.db") as connection:
         poll = connection.execute("SELECT * FROM polls JOIN options on polls.poll_id = options.poll_id WHERE polls.poll_id = ?", (poll_id,)).fetchall()
         title = poll[0][1]
-    return render_template("latest.html", title=title, poll=poll)
+    return render_template("latest.html", title=title, poll=poll, poll_id=poll_id, discord_uri=FINAL_URI)
 
 
-@app.route("/vote", methods=["POST"])
-def vote():
+@app.route("/vote/<int:poll_id>", methods=["POST"])
+def vote(poll_id):
     selected_vote = request.form.get("vote")
     with sqlite3.connect("data.db") as connection:
         connection.execute(
             "INSERT INTO votes (username, discriminator, vote, poll_id) VALUES (?, ?, ?, ?)",
-            (session['username'], session['discriminator'], selected_vote, 1)
+            (session['username'], session['discriminator'], selected_vote, poll_id)
         )
 
     return f"You voted for {selected_vote}"
@@ -80,9 +91,13 @@ def view_poll(poll_id):
         cursor = connection.execute("SELECT * FROM polls WHERE poll_id = ?", (poll_id,))
         poll = cursor.fetchone()
 
-        votes_cursor = connection.execute("SELECT vote, COUNT(*) as count FROM votes WHERE poll_id = ? GROUP BY vote;", (poll_id,))
+        votes_cursor = connection.execute("""
+            SELECT options.option_text, COUNT(votes.vote) as count, COUNT(votes.vote) * 100.0 / sum(count(votes.vote)) over() as percentage FROM options
+            LEFT JOIN votes on options.option_id = votes.vote
+            WHERE options.poll_id = ?
+            GROUP BY options.option_text;""", (poll_id,))
         votes = votes_cursor.fetchall()
-    return render_template("view_poll.html", poll=poll, votes=votes)
+    return render_template("view_poll.html", poll=poll, votes=votes, discord_uri=FINAL_URI)
 
 
 @app.route("/create_poll", methods=["GET", "POST"])
@@ -92,9 +107,26 @@ def create_poll():
         option_keys = ["option1", "option2", "option3", "option4"]
 
         with sqlite3.connect("data.db") as connection:
-            connection.execute("INSERT INTO polls (title) VALUES (?)", (title,))
-            last_inserted_id = connection.execute("SELECT last_insert_rowid();").fetchone()[0]
+            cursor = connection.execute("INSERT INTO polls (title, owner, owner_discriminator) VALUES (?, ?, ?)", (title, session['username'], session['discriminator']))
+            last_inserted_id = cursor.lastrowid
 
             options = [(request.form.get(option), last_inserted_id) for option in option_keys if request.form.get(option)]
             connection.executemany("INSERT INTO options (option_text, poll_id) VALUES (?, ?)", options)
-    return render_template("create_poll.html")
+
+        return redirect(url_for("poll", poll_id=last_inserted_id))
+    return render_template("create_poll.html", discord_uri=FINAL_URI)
+
+
+@app.route("/manage_polls")
+def manage_polls():
+    with sqlite3.connect("data.db") as connection:
+        cursor = connection.execute("SELECT * FROM polls WHERE owner = ? AND owner_discriminator = ?",
+                                    (session["username"], session["discriminator"]))
+
+        return render_template("manage_polls.html", polls=cursor.fetchall())
+
+
+@app.route("/logout")
+def logout():
+    session.clear()
+    return redirect(url_for("home"))
--- create_tables.py
+++ create_tables.py
@@ -1,6 +1,6 @@
 import sqlite3
 
 with sqlite3.connect("data.db") as connection:
-    connection.execute("CREATE TABLE options (option_id INTEGER PRIMARY KEY, option_text TEXT, poll_id INTEGER);")
-    connection.execute("CREATE TABLE polls (poll_id INTEGER PRIMARY KEY, title TEXT);")
-    connection.execute("CREATE TABLE votes (username TEXT, discriminator TEXT, vote TEXT, poll_id TEXT);")
+    connection.execute("CREATE TABLE IF NOT EXISTS options (option_id INTEGER PRIMARY KEY, option_text TEXT, poll_id INTEGER);")
+    connection.execute("CREATE TABLE IF NOT EXISTS polls (poll_id INTEGER PRIMARY KEY, title TEXT, owner TEXT, owner_discriminator TEXT);")
+    connection.execute("CREATE TABLE IF NOT EXISTS votes (username TEXT, discriminator TEXT, vote TEXT, poll_id TEXT);")
